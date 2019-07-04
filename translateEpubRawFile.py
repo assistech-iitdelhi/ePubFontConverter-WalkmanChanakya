d302f12d0851c4e1b45ce3b0c80fa320e13d3007
Himanshu Garg <hgarg@acm.org>
2019-07-04 09:11:19 +0530

    update string using replace_with

--- translateEpubRawFile.py
+++ translateEpubRawFile.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import bs4
 import sys
+import pdb
 import requests
 
 def api(text, font):
@@ -14,19 +15,17 @@ def english(s):
     # return true if any of the classes is present in the class list
     if s.has_attr('class'):
         for c in s['class']:
-            if c in s['class']:
+            if c in classes:
                 return True
     return False
 
 with open(sys.argv[1], 'r') as f:
     soup = bs4.BeautifulSoup(f, 'html.parser')
-    for d in soup.descendants:
+    for d in list(soup.body.descendants):
         if type(d) == bs4.element.NavigableString:
-            print d.parent.name, type(d.parent)
-            d = 'STRING'
-    #for span in [s for s in soup.find_all('span') if s.string and not english(s)]:
-    #    status, text = api(span.string, 'Walkman-Chanakya-901')
-    #    if status == 200:
-    #        span.string.replace_with(text)
+            if not english(d.parent):
+                status, text = api(unicode(d.string), 'Walkman-Chanakya-901')
+                if status == 200:
+                    d.replace_with(text)
 
     print(soup)
