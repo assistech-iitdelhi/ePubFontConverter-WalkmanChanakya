f5cdf04e3555dcad4cf3e240883754316811966e
Himanshu Garg <hgarg@acm.org>
2019-07-08 12:34:10 +0530

    run in temp dir to allow running with find

--- epubToEpub.sh
+++ epubToEpub.sh
@@ -1,42 +1,18 @@
 #!/bin/bash
-#SAVEIFS=$IFS
-#IFS=$(echo -en "\n\b")
-# non-unicode to unicode conversion 
-# of NCERT ePathShala epub file to unicode epub file
-# capturing the entire formatting
+#
+# Extract epub file and call translateEpubRawFile on contained html/xhtml
+#
 
-# Usage : ./runepub.sh <filename_without_extension> <option>
-# filename_without_extension : fname if document us fname.epub
-# option : 1 :- for walkman chanakya
-#		   2 :- for user uploaded mapping
+justname=$(basename "$1" .epub)
+justpath=$(dirname "$1")
 
-if [ -d $1 ] 
-then
-	rm -rf $1
-fi
-
-unzip $1.epub -d $1
-if [ $? -ne 0 ] 
-then
-	echo "---------- Corrupted file: "$1".epub -----------"
-	rm $1.epub
-	exit 0
-fi
-
-chap=$(find $1 -name '*.html' -o -name '*.xhtml') 
-echo $chap
-for ch in $chap
+unzip $1 -d "/tmp/$justname"
+chapters=$(find /tmp/$justname -name '*.html' -o -name '*.xhtml')
+for chapter in $chapters
 do
-	echo $ch
-	touch temp.txt
-	python translateEpubRawFile.py $ch > temp.txt
-	cp temp.txt $ch
-	rm temp.txt
+	echo "CONVERTING $chapter of $1"
+	python translateEpubRawFile.py $chapter > "/tmp/$justname.html"
+	cp "/tmp/$justname.html" $chapter
 done
 
-mkdir epubout
-cd $1
-zip -r $1n.epub *
-cp $1n.epub ../epubout/$1.epub
-cd ..
-rm -rf $1
+zip -r "$justpath/$justname-converted.epub" "/tmp/$justname"
